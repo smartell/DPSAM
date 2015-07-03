3fd3ded80df6c8558ef7858d2065518316c013bc
Steve Martell <martell.steve@gmail.com>
2015-07-03 15:05:36 -0700

    Moved dpsam.cpp inside shiny dir

--- R/shiny/helpers.R
+++ R/shiny/helpers.R
@@ -1,5 +1,19 @@
-library(shiny)
-library(markdown)
+# 
+# Load Libraries
+# 
+if(!require("shiny"))         install.packages("shiny")
+if(!require("shinythemes"))   install.packages("shinythemes")
+if(!require("ggplot2"))       install.packages("ggplot2")
+if(!require("reshape2"))      install.packages("reshape2")
+if(!require("Rcpp"))          install.packages("Rcpp")
+if(!require("markdown"))      install.packages("markdown")
+if(!require("plyr"))          install.packages("plyr")
+if(!require("dplyr"))         install.packages("dplyr")
+if(!require("grid"))          install.packages("grid")
+if(!require("shinydashboard"))install.packages("shinydashboard")
+if(!require("dygraphs"))      install.packages("dygraphs")
+sourceCpp("data/src/dpsam.cpp");
+
 
 .LIB        <- "data/lib/"
 .RFILES     <- list.files(.LIB,pattern="\\.[Rr]$")
